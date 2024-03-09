f034de664cfe53b9fdcf7bbfe1e8dc281f11c91f
DiffuseHyperion <95083897+DiffuseHyperion@users.noreply.github.com>
2024-03-09 18:06:24 +0800

    Addition: Add bcrypt salting Modify: Load .env correctly

--- src/databases/sqlite-database.ts
+++ src/databases/sqlite-database.ts
@@ -1,5 +1,6 @@
 import sqlite3, {Database} from "sqlite3"
 import IDatabase from './Database'
+import bcrypt from "bcrypt"
 
 class sqlite_database implements IDatabase {
     connection: Database;
@@ -16,8 +17,8 @@ class sqlite_database implements IDatabase {
 
     #initialize() {
         console.log("Initializing database")
-        this.connection.prepare(`CREATE TABLE IF NOT EXISTS users(user_id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, user TEXT NOT NULL, hash TEXT NOT NULL, admin BOOL NOT NULL);`).run()
-        this.connection.prepare(`CREATE TABLE IF NOT EXISTS experiences(experience_id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, description TEXT, owner_id INT, FOREIGN KEY (owner_id) REFERENCES users (user_id));`).run()
+        this.connection.prepare(`CREATE TABLE IF NOT EXISTS users (user_id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, user TEXT NOT NULL, hash TEXT NOT NULL, admin BOOL NOT NULL);`).run()
+        this.connection.prepare(`CREATE TABLE IF NOT EXISTS experiences (experience_id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, description TEXT, owner_id INT, FOREIGN KEY (owner_id) REFERENCES users (user_id));`).run()
         this.connection.prepare(`SELECT COUNT(*) FROM users;`).get((err, rows: any) => {
             if (err) {
                 throw err
@@ -25,8 +26,14 @@ class sqlite_database implements IDatabase {
             if (rows["COUNT(*)"] <= 0) {
                 console.warn("There was no user accounts registered! Automatically making a default admin account...")
                 // admin:admin
-                this.connection.prepare(`INSERT INTO users (user, hash, admin) VALUES ("admin", "8c6976e5b5410415bde908bd4dee15dfb167a9c873fc4bb8a81f6f2ab448a918", 1);`).run()
-                console.log("Default user created! Log in with admin:admin")
+                bcrypt.hash("admin", process.env.SALT_ROUNDS || 8, (err, hash) => {
+                    if (err) {
+                        throw err
+                    }
+                    this.connection.prepare(`INSERT INTO users (user, hash, admin) VALUES ("admin", ?, 1);`).run(hash)
+                    console.log("Default user created! Log in with admin:admin")
+                })
+
             }
         })
     }
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,10 @@
 import express, {Express} from "express"
+import dotenv from "dotenv"
 import IDatabase from "./databases/Database"
 import indexRouter from "./routes/index"
 import usersRouter from "./routes/users/index"
+
+dotenv.config()
 
 const app: Express = express()
 const port = process.env.PORT || 3000
